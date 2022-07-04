81a7fe479c18110467aa8465a09b8e91c6dd4e3f
Berkcan Gümüşışık <gumusisikberkcan@gmail.com>
2022-07-04 08:17:29 +0300

    Miuul Summer Camp started.

---
+++ Veri Bilimi için Python Programlama/Çalışma Ortamı Ayarları/ilkAdimlar.py
@@ -0,0 +1,31 @@
+# print() ekrana yazı yazmayı sağlar.
+print("Hello World")
+
+# Karakter Dizileri(Strings) ve Sayılar(Numbers)
+print(9)
+print("Hello AI Era")
+
+'''
+String ile number arasındaki farka bakıldığında stringler "" içerisinde yazılırken sayısal ifadeler boş yazılmaktadır.
+'''
+
+# type() fonksiyonu nesnenin(şey denmez nesne denir) veri tipini bulmayı sağlar (int, string, float, bool)
+print(type(9))
+print(type(9.2))
+print(type("Berkcan"))
+
+#Atamalar ve Değişkenler (Assignments & Variables)
+'''
+Değişkenler sayesinde oluşturduğumuz veriyi programın ilerleyen kısımlarında da kullanmayı sağlıyor.
+Atama yaparken = ifadesi kullanılır.
+'''
+
+a = 9
+print(a)
+b = "Hello AI Era"
+print(b)
+c = 10
+print(a * c)
+print(a * 10)
+d = a - c
+print(d)
